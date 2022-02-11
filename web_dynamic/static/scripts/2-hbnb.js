6f4c60ecf1ee908c3746015077d8740265d30939
anversa-pro <angelavergaras@gmail.com>
2022-02-11 03:10:16 +0000

    [MODIFY] change status validation

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,6 +1,6 @@
 const tag_list = {};
 
-$(document).ready(function (){
+$(document).ready(function () {
     $('div.amenities input:checkbox').change(function () {
         if ($(this).is(':checked')) {
             tag_list[$(this).attr('data-id')] = $(this).attr('data-name');
@@ -13,10 +13,10 @@ $(document).ready(function (){
         $('div.amenities h4').text(names.join(', '));
     });
 
-   const available_status = $('div#api_status');
-   $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (statusCode) {
-       if (statusCode === 200) {
-           available_status.addClass('available');
+    const available_status = $('#api_status');
+    $.ajax('http://0.0.0.0:5001/api/v1/status/').done(function (statusCode) {
+        if (statusCode.status === "OK") {
+            available_status.addClass('available');
         } else {
             available_status.removeClass('available');
         }
